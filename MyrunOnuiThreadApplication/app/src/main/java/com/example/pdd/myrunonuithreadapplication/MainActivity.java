2f728b7f1c79dfb740dd7ee85fca752b11ee4bbf
renjunxian <you@example.com>
2020-03-31 22:25:39 +0800

    runOnUiThread的使用小Demo

---
+++ MyrunOnuiThreadApplication/app/src/main/java/com/example/pdd/myrunonuithreadapplication/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.pdd.myrunonuithreadapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button01);
+        textView = findViewById(R.id.text);
+
+        /*button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            Thread.sleep(200);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        textView.setText("text change!!!!");
+                    }
+                }).start();
+            }
+        });*/
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            Thread.sleep(5000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                textView.setText("text change!!!!");
+                            }
+                        });
+                    }
+                }).start();
+
+
+            }
+        });
+    }
+}
